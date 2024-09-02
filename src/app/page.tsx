2af7a4d221676b6230c4bacef467b6908524f670
ts7n <65257768+ts7n@users.noreply.github.com>
2024-09-02 02:43:16 -0500

    update github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,7 +100,7 @@ export default function Home() {
           <div className="flex justify-between">
             <div />
             <a
-              href="https://github.com/ts7n/ftc-chat"
+              href="https://github.com/ts7n/ftc-manual"
               className="transition duration-200 flex gap-2 rounded-md bg-stone-100 px-3 py-2 text-sm font-semibold text-stone-600 hover:bg-stone-200"
             >
               <svg className="w-5 h-5 my-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
