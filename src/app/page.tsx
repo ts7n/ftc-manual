108a015ea686b8b01c42d1199b5b9409f4ad3631
ts7n <65257768+ts7n@users.noreply.github.com>
2024-09-02 15:28:00 -0500

    github now opens in new tab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -109,6 +109,7 @@ export default function Home() {
             <div />
             <a
               href="https://github.com/ts7n/ftc-manual"
+              target="_blank"
               className="transition duration-200 flex gap-2 rounded-md bg-stone-100 px-3 py-2 text-sm font-semibold text-[#24292e] hover:bg-stone-200"
             >
               <svg className="w-5 h-5 my-auto fill-[#24292e]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
